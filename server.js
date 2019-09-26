cb659e2dfc2569230e84ab477c0e8684597c31be
NadiaMok <mokhirevanadezda@gmail.com>
2019-09-26 11:19:12 -0400

    initial commit

---
+++ server.js
@@ -0,0 +1,62 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+var mongodb = require("mongodb");
+var ObjectID = mongodb.ObjectID;
+
+var CONTACTS_COLLECTION = "contacts";
+
+var app = express();
+app.use(bodyParser.json());
+
+// Create a database variable outside of the database connection callback to reuse the connection pool in your app.
+var db;
+
+// Connect to the database before starting the application server.
+mongodb.MongoClient.connect(process.env.MONGODB_URI || "mongodb://root:toor@contacts-shard-00-00-f3aud.gcp.mongodb.net:27017,contacts-shard-00-01-f3aud.gcp.mongodb.net:27017,contacts-shard-00-02-f3aud.gcp.mongodb.net:27017/test?ssl=true&replicaSet=contacts-shard-0&authSource=admin&retryWrites=true&w=majority", function (err, client) {
+  if (err) {
+    console.log(err);
+    process.exit(1);
+  }
+
+  // Save database object from the callback for reuse.
+  db = client.db();
+  console.log("Database connection ready");
+
+  // Initialize the app.
+  var server = app.listen(process.env.PORT || 8080, function () {
+    var port = server.address().port;
+    console.log("App now running on port", port);
+  });
+});
+
+// Generic error handler used by all endpoints.
+function handleError(res, reason, message, code) {
+    console.log("ERROR: " + reason);
+    res.status(code || 500).json({"error": message});
+  }
+  
+  /*  "/api/contacts"
+   *    GET: finds all contacts
+   *    POST: creates a new contact
+   */
+  
+  app.get("/api/contacts", function(req, res) {
+  });
+  
+  app.post("/api/contacts", function(req, res) {
+  });
+  
+  /*  "/api/contacts/:id"
+   *    GET: find contact by id
+   *    PUT: update contact by id
+   *    DELETE: deletes contact by id
+   */
+  
+  app.get("/api/contacts/:id", function(req, res) {
+  });
+  
+  app.put("/api/contacts/:id", function(req, res) {
+  });
+  
+  app.delete("/api/contacts/:id", function(req, res) {
+  });
